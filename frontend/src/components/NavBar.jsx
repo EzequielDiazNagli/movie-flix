ea0c737744a55d21967a966e05ee044a390fa9a0
Hector Salgado <hectorsalgadomedina@gmail.com>
2022-09-06 15:57:46 -0300

    user

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,40 +3,41 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
-import {Link as LinkRouter, useNavigate} from "react-router-dom"
+import { Link as LinkRouter, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import moviesActions from "../redux/actions/moviesActions"
+import moviesActions from "../redux/actions/moviesActions";
 import userActions from "../redux/actions/userActions";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import logo from "../assets/logo2.png"
+import logo from "../assets/logo2.png";
+import { FaUser } from "react-icons/fa";
 
-const navigation = [ 
-  { name: "Home", href: "/", current: true },
-];
+const navigation = [{ name: "Home", href: "/", current: true }];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function NavBar() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [search, setSearch] = useState("");
-  const loggedUser = useSelector(store => store.userReducer.loggedUser)
-  const favorites = useSelector(store => store.userReducer.favorites)
+  const loggedUser = useSelector((store) => store.userReducer.loggedUser);
+  const favorites = useSelector((store) => store.userReducer.favorites);
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
   }
 
-  useEffect(()=> {
-    dispatch(moviesActions.filterMovies(search))
+  useEffect(() => {
+    dispatch(moviesActions.filterMovies(search));
     // eslint-disable-next-line
-  },[search])
-  
+  }, [search]);
 
   return (
-    <Disclosure as="nav" className="bg-gray-800  sm:bg-transparent absolute z-10 w-full">
+    <Disclosure
+      as="nav"
+      className="bg-gray-800  sm:bg-transparent absolute z-10 w-full"
+    >
       {({ open }) => (
         <>
           <div className="max-w-screen-2xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -60,7 +61,7 @@ export default function NavBar() {
                     alt="Workflow"
                   />
                 </div>
-                <div className="hidden sm:block sm:ml-6" >
+                <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <LinkRouter
@@ -82,35 +83,49 @@ export default function NavBar() {
               </div>
               <div className="gap-2 absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-0 sm:pr-0">
                 {/* Search*/}
-                  <div className="buscador">
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                          <input type="text" name="name" placeholder="Search..." onKeyUp={e => setSearch(e.target.value)}/>
-                        </label>
-                    </form>
-                  </div>
-              <LinkRouter to="/favorites">
-                <Badge badgeContent={favorites.length} color="primary">
-                  {loggedUser !== null ?
-                    favorites.length > 0 ?
-                      <AiFillHeart className="text-white text-2xl"/>
-                      :
-                      <AiOutlineHeart className="text-2xl"/>
-                    :
-                    <AiOutlineHeart className="text-2xl"/>
-                  }
-                </Badge>
-              </LinkRouter>
+                <div className="buscador">
+                  <form onSubmit={handleSubmit}>
+                    <label>
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Search..."
+                        onKeyUp={(e) => setSearch(e.target.value)}
+                      />
+                    </label>
+                  </form>
+                </div>
+                <LinkRouter to="/favorites">
+                  <Badge badgeContent={favorites.length} color="primary">
+                    {loggedUser !== null ? (
+                      favorites.length > 0 ? (
+                        <AiFillHeart color="white" fontSize={30} />
+                      ) : (
+                        <AiOutlineHeart color="white" fontSize={30} />
+                      )
+                    ) : (
+                      <AiOutlineHeart color="white" fontSize={30} />
+                    )}
+                  </Badge>
+                </LinkRouter>
                 {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative z-10">
                   <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full">
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      {loggedUser ? (
+                        <FaUser
+                          color="white"
+                          fontSize={30}
+                          style={{ background: "black", borderRadius: 50 }}
+                        />
+                      ) : (
+                        <FaUser
+                          color="black"
+                          fontSize={30}
+                          style={{ background: "white", borderRadius: 50 }}
+                        />
+                      )}
                     </Menu.Button>
                   </div>
                   <Transition
@@ -123,50 +138,51 @@ export default function NavBar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {loggedUser ?
-                      <Menu.Item >
-                      {({ active }) => (
-                        <LinkRouter
-                        onClick={() => dispatch(userActions.userLogout())}
-                          to={"/"}
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
+                      {loggedUser ? (
+                        <Menu.Item>
+                          {({ active }) => (
+                            <LinkRouter
+                              onClick={() => dispatch(userActions.userLogout())}
+                              to={"/"}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Logout
+                            </LinkRouter>
                           )}
-                        >
-                          Logout
-                        </LinkRouter>
+                        </Menu.Item>
+                      ) : (
+                        <>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <LinkRouter
+                                to={"/login"}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Login
+                              </LinkRouter>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <LinkRouter
+                                to={"/register"}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Register
+                              </LinkRouter>
+                            )}
+                          </Menu.Item>
+                        </>
                       )}
-                    </Menu.Item> : 
-                    <>
-                    <Menu.Item>
-                    {({ active }) => (
-                      <LinkRouter
-                        to={"/login"}
-                        className={classNames(
-                          active ? "bg-gray-100" : "",
-                          "block px-4 py-2 text-sm text-gray-700"
-                        )}
-                      >
-                        Login
-                      </LinkRouter>
-                    )}
-                  </Menu.Item>
-                    <Menu.Item>
-                    {({ active }) => (
-                      <LinkRouter
-                        to={"/register"}
-                        className={classNames(
-                          active ? "bg-gray-100" : "",
-                          "block px-4 py-2 text-sm text-gray-700"
-                        )}
-                      >
-                        Register
-                      </LinkRouter>
-                  )}
-                  </Menu.Item>
-                  </>
-                    }
                     </Menu.Items>
                   </Transition>
                 </Menu>
